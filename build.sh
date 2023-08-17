257a801caf84da2daccf15f3240bb080cb7858b2
Adam Thunberg <adam@honung.org>
2023-08-17 18:14:21 +0200

    :)

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-wget https://github.com/mullvad/mullvadvpn-app/releases/download/2023.4/MullvadVPN-2023.4_x86_64.rpm -O /tmp/mullvad.rpm
-rpm-ostree install /tmp/mullvad.rpm
-
 rpm-ostree install \
 		   fish \
 		   darkman \
@@ -29,3 +26,7 @@ rpm-ostree override remove \
 
 mkdir -p /usr/etc/pki/containers
 cp /usr/share/silverred/cosign.pub /usr/etc/pki/containers/silverred.pub
+
+systemctl enable rpm-ostreed-automatic.timer
+systemctl enable flatpak-system-update.timer
+systemctl --global enable flatpak-user-update.timer
