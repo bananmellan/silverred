47dc218f8834951a7efc52ba1b3a71304ebaaa24
Adam Thunberg <grandma_smoky440@slmail.me>
2023-12-27 14:35:01 +0100

    Build and install Emacs as part of the build script.

---
+++ build-emacs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if ! [ $DISTROBOX_ENTER_PATH ]; then
+    distrobox create -Y -i registry.fedoraproject.org/fedora-toolbox:39 \
+              --name emacs-build
+
+    wget http://ftp.acc.umu.se/mirror/gnu.org/gnu/emacs/emacs-29.1.tar.xz \
+         -O /tmp/emacs.tar.xz
+    cd /tmp
+    tar xvf emacs.tar.xz
+    distrobox enter -Y emacs-build -- /tmp/build-emacs.sh
+    cd /tmp/emacs-*/
+e    sudo make install
+else
+    cd /tmp/emacs-*/
+    yum builddep -y emacs
+    ./configure --with-native-compilation --with-tree-sitter --with-pgtk
+    make -j(nproc)
+fi
