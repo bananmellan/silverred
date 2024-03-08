5146927d3310bb62021d5dbfbe704deeb09a78b0
Adam <grandma_smoky440@slmail.me>
2024-03-08 20:02:25 +0100

    :)

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+wget https://github.com/mullvad/mullvadvpn-app/releases/download/2023.4/MullvadVPN-2023.4_x86_64.rpm -O /tmp/mullvad.rpm
+rpm-ostree install /tmp/mullvad.rpm
+
 rpm-ostree install \
 		   fish \
 		   darkman \
