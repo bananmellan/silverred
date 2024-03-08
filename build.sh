3e8f11600d2062e9ad33d5a9962a2350bf4d4955
Adam <grandma_smoky440@slmail.me>
2024-03-08 20:03:43 +0100

    :)

--- build.sh
+++ build.sh
@@ -9,20 +9,20 @@ shopt -s extglob
 # Remove unnecessary (non-free) repos and keys.
 REPO_DIR=/etc/yum.repos.d
 if [ -d $REPO_DIR ]; then
-	pushd $REPO_DIR
-	echo Removing nonfree repos:
-	rm -vf !(fedora*.repo)
-	popd
+    pushd $REPO_DIR
+    echo Removing nonfree repos:
+    rm -vf !(fedora*.repo)
+    popd
 else
-	echo Repo directory not found! Skipping removal of non-free repos.
+    echo Repo directory not found! Skipping removal of non-free repos.
 fi
 
 FUSION_KEY_DIR=/usr/share/distribution-gpg-keys/rpmfusion
 if [ -d $FUSION_KEY_DIR ]; then
-	echo Removing RPMfusion keys.
-	rm -rf $FUSION_KEY_DIR
+    echo Removing RPMfusion keys.
+    rm -rf $FUSION_KEY_DIR
 else
-	echo RPMfusion key directory not found, skipping removal of these keys.
+    echo RPMfusion key directory not found, skipping removal of these keys.
 fi
 
 INCLUDED_PKGS=$(cat /tmp/pkgs.yaml | shyaml get-values include | uniq)
@@ -30,30 +30,27 @@ EXCLUDED_PKGS=$(cat /tmp/pkgs.yaml | shyaml get-values exclude | uniq)
 
 # Remove only packages that are actually installed.
 if [[ "${#EXCLUDED_PKGS}" -gt 0 ]]; then
-	EXCLUDED_PKGS=$(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PKGS})
+    EXCLUDED_PKGS=$(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PKGS})
 fi
 
 # If there are packages to install AND remove, run it all in one go.
 if [[ "${#INCLUDED_PKGS}" -gt 0 && "${#EXCLUDED_PKGS}" -gt 0 ]]; then
-	rpm-ostree override \
-			   remove $EXCLUDED_PKGS \
-			   $(printf -- "--install=%s " $INCLUDED_PKGS)
+    rpm-ostree override \
+               remove $EXCLUDED_PKGS \
+               $(printf -- "--install=%s " $INCLUDED_PKGS)
 
 # If there are only packages to install.
 elif [[ "${#INCLUDED_PKGS}" -gt 0 && "${#EXCLUDED_PKGS}" -eq 0 ]]; then
-	rpm-ostree install $INCLUDED_PKGS
+    rpm-ostree install $INCLUDED_PKGS
 
 # If there are only packages to remove (override).
 elif [[ "${#INCLUDED_PKGS}" -eq 0 && "${#EXCLUDED_PKGS}" -gt 0 ]]; then
-	rpm-ostree override remove $EXCLUDED_PKGS
+    rpm-ostree override remove $EXCLUDED_PKGS
 
 # If there are no intended package actions.
 else
-	echo No packages to install.
+    echo No packages to install.
 fi
-
-# Ensure ld exe is present in path
-ln -s /usr/bin/ld.lld /usr/local/bin/ld
 
 # Make fish the default shell
 sed -i 's/bin\/bash/bin\/fish/' /usr/etc/default/useradd
